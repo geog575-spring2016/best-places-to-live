ad80b0578374d19b268e521fc618a9489ae8d92f
ncrump11 <ncrump11@yahoo.com>
2016-04-18 12:57:30 -0500

    Boilerplate added

---
+++ js/main.js
@@ -0,0 +1,6 @@
+/* 575 boilerplate main.js */
+var map = d3.select("#map")
+            .append("svg")
+            .attr("class", "map")
+            .attr("width", width)
+            .attr("height", height);
