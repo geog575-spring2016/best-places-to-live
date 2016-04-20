eddabbc9411826bfe0a8608ff4e37e3cf5ef4fd2
Alex Zarley <azarley@wisc.edu>
2016-04-20 13:45:32 -0500

    Added defer/callback function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
 (function(){
 
-window.onload = setMap();
+window.onload = setPage();
           //pseudo-global variables
+
+function setPage() {
+    //set variable to use queue.js to parallelize asynchronous data loading
+    var q = d3_queue.queue();
+
+    //use queue to retrieve data from all files
+    q
+        .defer(d3.json, "data/US.topojson")//load states\
+        //add defer function for cities
+        //add defer function for data
+        .await(callback);
+
+    //function called once data has been retrieved from all .defer lines
+    function callback(error, statesData){
+        //convert topojsons into geojson objects
+        var states = topojson.feature(statesData, statesData.objects.states).features;
+
+        createMap(states);
+    }
+};
+
+function createMap(states) {
+    
+}
+
+
+
 
 
 
