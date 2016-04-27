270cd7157e41527a52669c39b12d7f25f5acb2d2
Alex Zarley <azarley@wisc.edu>
2016-04-27 15:45:40 -0500

    Changed CSS and update prepros config
    Map appears under attribute panel; need to rework size of map and
    attribute panel to fit on a smallish window

--- js/main.js
+++ js/main.js
@@ -18,10 +18,13 @@ function callback(error, statesData, citiesData, attData){
     // console.log(attData);
     //convert topojsons into geojson objects
     var states = topojson.feature(statesData, statesData.objects.US).features;
-
+    var cities = topojson.feature(citiesData, citiesData.objects.collection).features;
 
     // createDefaultAtts(attData);
     createAttPanel(attData);
+
+    createMap(states, cities);
+
 }
 
 
@@ -256,6 +259,72 @@ function calcMinMax(attData, attribute){
     //return values as an array
     return [min, max]
 };
+
+function createMap(states, cities) {
+
+    var mapWidth = 0.75;
+    var width = window.innerWidth * mapWidth;
+    var height = window.innerHeight;
+
+    var map = d3.selectAll("body")
+        .append("svg")
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height);
+
+    var g = map.append("g");
+
+    //no idea how this works but it does
+    //will probably need to change it once we decide how big
+    //"mapWidth" is actually going to be
+    var projection = d3.geo.mercator()
+        .scale((width - 1)/2)
+        .translate([width*1.3, height]);
+
+    var zoom = d3.behavior.zoom()
+        .scaleExtent([1, 8])
+        .on("zoom", zoomed);
+
+    var path = d3.geo.path()
+        .projection(projection);
+
+    //add the states to the map
+    g.selectAll(".states")
+            .data(states)
+            .enter()
+            .append("path")
+            .attr("class", "us_states")
+            .attr("d", path);
+
+    map
+        .call(zoom)
+        .call(zoom.event);
+
+    //function to control when the user zooms
+    function zoomed() {
+      g.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+      g.selectAll("circle")
+            .attr("d", path.projection(projection));
+    }
+
+    //array to store coordinates for every city
+    //there might be a better/more efficient way than to iterate through it like
+    //this but for now it works and thats good enough
+    var allCoordinates = [];
+
+    g.selectAll(".circles")
+            .data(cities)
+            .enter()
+            .append("circle")
+            .attr("class", function(d) {return d.properties.ID})
+            .attr("cx", function (d) { console.log(d); return projection(d.geometry.coordinates)[0]; })
+            .attr("cy", function (d) { return projection(d.geometry.coordinates)[1]; })
+            .attr("r", "6px")
+            .attr("fill", "white")
+            .attr("stroke", "black")
+            .attr("stroke-width", "2px");
+}
+
 
 function createAttID(d, rankData) {
     //put d in array because addUnderscores only takes an array
